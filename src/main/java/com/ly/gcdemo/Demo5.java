33ba3df2258135544a6fdbda82cc199e610c7678
Luoyun <luoyun@iplas.com.cn>
2019-09-19 09:38:12 +0800

    模拟进入老年代

--- src/main/java/com/ly/gcdemo/Demo5.java
+++ src/main/java/com/ly/gcdemo/Demo5.java
@@ -1,25 +1,17 @@
 package com.ly.gcdemo;
 
 /**
- *  新生代GC (动态年龄判断， 进入老年代）
+ * 一次minor gc后，有一部分对象保存在survivor中，一部分对象进入老年代
  */
-public class Demo3 {
+public class Demo5 {
     public static void main(String[] args) {
         byte[] array1 = new byte[2 * 1024 * 1024];
         array1 = new byte[2*1024*1024];
         array1 = new byte[2*1024*1024];
-        array1 = null;
 
         byte[] array2 = new byte[128 * 1024];
+        array2 = null;
 
         byte[] array3 = new byte[2 * 1024 * 1024];
-        array3 = new byte[2*1024*1024];
-        array3 = new byte[2*1024*1024];
-        array3 = new byte[128*1024];
-        array3 = null;
-
-        byte[] array4 = new byte[2 * 1024 * 1024];
-        
-
     }
 }
